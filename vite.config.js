f905de1836dacfa24ea8c5f4bff6827b9ea4d138
Acaibird <lian.wk04201@foxmaill.com>
2023-08-05 14:19:06 -0700

    chrod:项目代码格式化

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 export default defineConfig({
-  outDir: path.resolve(__dirname, ""),
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    }
-  },
-  server: {
-    open: '/src/pages/index/index.html', //设置项目启动打开的首页
-  },
-  
+    outDir: path.resolve(__dirname, ""),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': '/src',
+        }
+    },
+    server: {
+        open: '/src/pages/index/index.html', //设置项目启动打开的首页
+    },
+
 })
