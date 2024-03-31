c1ed4a6637751d1469dc1813aa7aee6a7d48a48c
Acaibird <newcoder@icloud.com>
2024-04-01 00:19:54 +0800

    Revert "feat:删除原有components文件夹"
    This reverts commit ae5c3396cec5a4f94793608e62cb0a8019ee5126.

---
+++ src/components/musicComment/index.vue
@@ -0,0 +1,133 @@
+<script setup>
+import {ref, onMounted} from 'vue';
+import {useRouter, useRoute} from 'vue-router';
+import {getSongComment, commentSong} from '/src/api/api.js';
+import {ElMessage} from 'element-plus';
+
+const router = useRouter();
+const route = useRoute();
+console.log(document.getElementsByClassName("mt-[20px]").length)
+// 获取路由参数
+const musicName = route.params.musicName;
+
+// 读取浏览器存储的歌曲信息
+const time = localStorage.musicTimeLength;
+const singer = localStorage.singerName_album;
+
+// 我的评论
+let yourComment = ref('');
+let musicComment = ref([]);
+
+// 新增我的评论
+function handleComment() {
+    if (yourComment.value.trim().length < 5) {
+        ElMessage.warning('评论至少需要五个字符以上,请重新输入')
+        return;
+    }
+    commentSong({
+        musicName: musicName,
+        comment: yourComment.value.trim(),
+        userName: localStorage.name
+    }).then(
+        (res) => {
+            getSongComment({
+                params: {
+                    name: musicName
+                }
+            }).then((res) => {
+                musicComment.value = res.data;
+                console.log(musicComment.value);
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    ).catch((err) => {
+        console.log(err);
+    })
+}
+
+// 页面加载,拿到歌曲评论列表
+onMounted(() => {
+    getSongComment({
+        params: {
+            name: musicName
+        }
+    }).then((res) => {
+        musicComment.value = res.data;
+        console.log(musicComment.value);
+    }).catch((err) => {
+        console.log(err)
+    })
+});
+</script>
+<template>
+    <div class="flex flex-col items-center font-sans">
+        <div class="w-[1000px] min-h-[600px] mt-[20px] box-border p-[20px] rounded-lg ">
+            <!-- 歌曲详情 -->
+            <div class="m-full h-[200px] bg-yellow-50 flex flex-row ">
+                <span class="box-border p-[10px] w-[200px] h-[200px] bg-yellow-50 inline-block">
+                    <img class=" w-full h-full" src="/comment.png"/>
+                </span>
+                <span class="box-border flex flex-col w-full p-[10px]">
+                    <p class=" self-center">
+                        {{ musicName }}
+                    </p>
+                    <div class="flex flex-row justify-between mt-[20px] mb-[10px]">
+                        <p>
+                            歌手/专辑: {{ singer }}
+                        </p>
+                        <p>
+                            歌曲时长: {{ time }}
+                        </p>
+                    </div>
+                    <el-input v-model="yourComment" :rows="2" type="textarea"
+                              placeholder="留下评论,共探讨音乐的美好!      注意:shift+enter换行,enter评论!" class="font-san"
+                              @keydown.enter="handleComment"/>
+                </span>
+            </div>
+            <!-- 歌曲评论 -->
+            <div class="bg-[#A0F6D2] p-4 overflow-auto h-[600px] panel">
+                <div class="mt-[20px] border-b-2 border-[#72DFD0]" v-for="(item,index) of musicComment" :key="index">
+                    <div class="">
+                        <div class="relative">
+                            <span class="text-yellow-900">
+                                用户名:{{ musicComment[musicComment.length - index - 1].userName }}
+                            </span>
+                            <span class="font-sans  text-[12px]/[12px] absolute right-0">
+                                评论时间: {{ musicComment[musicComment.length - index - 1].date }}
+                            </span>
+                        </div>
+                        <div class="min-h-[20px] w-full  rounded-md py-[10px]  font-serif">
+                            {{ musicComment[musicComment.length - index - 1].commentContent }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<style scoped>
+/*  textarea 高度设置 */
+:deep(.el-textarea) {
+    height: 100% !important;
+}
+
+:deep(.el-textarea__inner) {
+    height: 100% !important;
+    color: black;
+}
+
+:deep(.el-textarea__inner::placeholder) {
+    color: #cbd5e1;
+    @apply text-slate-300;
+}
+
+.panel {
+    background-image: linear-gradient(25deg, #026a92, #6588ac, #a3a7c7, #dec6e2);
+    background-image: linear-gradient(25deg, #ed4c79, #ea8c66, #e1c04c, #d0ef13);
+    background-image: linear-gradient(25deg, #149cc9, #7daaa3, #aab87a, #cbc849)
+}
+
+
+</style>
---
+++ src/components/vip/index.vue
@@ -0,0 +1,311 @@
+<script setup>
+import {shopVip, payVip, getVipInfo, getComboList} from '@/api/api.js'
+import {ref, onMounted} from 'vue'
+import {useRouter, useRoute} from 'vue-router'
+import {ElMessage} from 'element-plus'
+import {vipInfo} from '@/pinia/store.js'
+import {Client} from '@stomp/stompjs';
+
+
+const router = useRouter()
+const route = useRoute()
+
+const name = localStorage.getItem("name")
+const isVip = ref(false)
+const time = new Date()
+const price = ref(180)
+
+const curYear = ref(time.getFullYear())
+const curMonth = ref(time.getMonth())
+const curTime = ref(curYear.value + "-" + curMonth.value + "-" + time.getDate())
+const destTime = ref((curYear.value + 1) + "-" + (curMonth.value) + "-" + time.getDate())
+
+const arr = ref([1, 0, 0, 0])
+
+function funcClickSelect(t, p, index) {
+    // 改变vip时间
+    price.value = p
+    let m = curMonth.value + t
+    let y = curYear.value
+    let res = new String("")
+    if (m / 12 > 1) {
+        y++
+        m -= 12
+    }
+    destTime.value = y + "-" + m + "-" + time.getDate()
+
+    // 改变样式
+    let temp = [0, 0, 0, 0]
+    temp[index] = 1
+    arr.value = temp
+}
+
+const selectPayMethods = ref(true)
+
+function funcSelectPay(index) {
+    if (index === 1) {
+        selectPayMethods.value = true;
+    } else {
+        selectPayMethods.value = false;
+    }
+}
+
+function funcClickPay() {
+    payVip({price: price.value, name: localStorage.getItem("name")}).then(res => {
+        shopVip({
+            username: localStorage.getItem('name'),
+            startDate: curTime.value,
+            endDate: destTime.value
+        }).then(response => {
+            ElMessage.success("支付成功")
+        }).then(error => {
+            console.error(error.data)
+            ElMessage.error("服务器内部错误,请稍后再试")
+        })
+    }).catch(error => {
+        if (error.response.status == 400) {
+            let ok = confirm("余额不足,请充值")
+            if (ok) {
+                ElMessage.success("即将跳转到充值页面")
+                router.push({path: '/topin'})
+            }
+        } else {
+            ElMessage.error("服务器内部错误,请稍后再试")
+        }
+    })
+}
+
+// 套餐信息
+let comboInfo = ref([])
+{
+    watch(//这个时候不能用.value且必须是深度监听，这种写法不仅可以监听数组本身的变化，也可以监听 数组元素的变化
+        comboInfo,
+        () => {
+            console.log('空数组变化了')
+        },
+        {
+            deep: true
+        }
+    )
+}
+const VipInfo = vipInfo()
+onMounted(() => {
+    getVipInfo({
+        username: localStorage.getItem('name')
+    }).then(res => {
+        isVip.value = res.data.vipStatus;
+    }).catch(e => {
+        console.error(e)
+        console.log("获取vip状态错误")
+    })
+    getComboList().then(res => {
+        VipInfo.$patch((state) => {
+                state.vipinfo = res.data
+            }
+        )
+        comboInfo.value = VipInfo.vipinfo
+    }).catch(e => {
+        console.log(e)
+    })
+})
+</script>
+<template>
+    <div class="grid grid-cols-[360px_auto_360px] h-full py-[60px] w-full">
+        <div class="cols-start-2 col-end-3 ] h-[600px] rounded-lg grid grid-cols-[200px_auto]">
+            <div class="col-span-1 h-full border-r-2 border-black bg-[#EF6351]">
+                <div class="flex flex-col items-center">
+                    <image src="/vip.png" class="w-full h-full">
+                    </image>
+                    <div class="flex flex-col items-center gap-y-5">
+                        <span class="mt-[20px] font-DiyChineseFont">
+                            千万曲库
+                        </span>
+                        <span>
+                            不限量下载
+                        </span>
+                        <span>
+                            无损音质
+                        </span>
+                        <span>
+                            尊贵身份标识
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-1 h-full px-[20px] bg-[#F38375] relative">
+                <div class="w-full flex  py-[20px]  border-fuchsia-200 border-dashed border-b-[1px]">
+                    <span class="w-[110px]">
+                        开通账号
+                    </span>
+                    <span class="mr-[20px]">
+                        {{ name }}
+                    </span>
+                    <span class="p-[3px] bg-yellow-300 rounded-lg text-[12px] ">
+                        已开通豪华VIP
+                    </span>
+                    <span>
+
+                    </span>
+                </div>
+                <div class="py-[20px] gap-[20px] flex flex-col  w-full">
+                    <div class="">
+                        <span class="w-[90px] inline-block">
+                            产品类型
+                        </span>
+                        <span>
+                            （豪华VIP特权，开通即送音乐包）
+                        </span>
+                    </div>
+                    <div class=" h-[60px] flex">
+                        <span class="w-[90px] inline-block mr-[14px]">
+                            开通时长
+                        </span>
+                        <span class="flex  gap-x-[20px]" v-if="comboInfo.length > 0">
+                            <span class="border-[1px] flex items-center flex-col justify-center p-[10px] border-red-300"
+                                  :class="{
+                                    'border-white': arr[0]
+                                }" @click="funcClickSelect(12, 180, 0)">
+                                <div class="flex gap-2">
+                                    <span>
+                                        {{ comboInfo[0].name }}
+                                    </span>
+                                    <span class=" text-[#f43f5e]">
+                                        {{ comboInfo[0].price_now }}元
+                                    </span>
+                                </div>
+                                <div class="text-[10px]">
+                                    <span class="">
+                                        原价
+                                    </span>
+                                    <del class="">
+                                        {{ comboInfo[0].price_origin }}元
+                                    </del>
+                                </div>
+                            </span>
+                            <span class="border-[1px] flex items-center flex-col justify-center p-[10px] border-red-300"
+                                  @click="funcClickSelect(6, 90, 1)" :class="{
+                                    'border-white': arr[1]
+                                }">
+                                <div class="flex gap-2">
+                                    <span>
+                                        {{ comboInfo[0].name }}
+                                    </span>
+                                    <span class=" text-[#f43f5e]">
+                                        {{ comboInfo[1].price_now }}元
+                                    </span>
+                                </div>
+                                <div class="text-[10px]">
+                                    <span class="">
+                                        原价
+                                    </span>
+                                    <del class="">
+                                        {{ comboInfo[1].price_origin }}元
+                                    </del>
+                                </div>
+                            </span>
+                            <span class="border-[1px] flex items-center flex-col justify-center p-[10px] border-red-300 "
+                                  @click="funcClickSelect(3, 45, 2)" :class="{
+                                    'border-white': arr[2]
+                                }">
+                                <div class="flex gap-2">
+                                    <span>
+                                        {{ comboInfo[2].name }}
+                                    </span>
+                                    <span class=" text-[#f43f5e]">
+                                        {{ comboInfo[2].price_now }}元
+                                    </span>
+                                </div>
+                                <div class="text-[10px]">
+                                    <span class="">
+                                        原价
+                                    </span>
+                                    <del class="">
+                                        {{ comboInfo[2].price_origin }}元
+                                    </del>
+                                </div>
+                            </span>
+                            <span class="border-[1px] flex items-center flex-col justify-center p-[10px] border-red-300"
+                                  @click="funcClickSelect(1, 15, 3)" :class="{
+                                    'border-white': arr[3]
+                                }">
+                                <div class="flex gap-2">
+                                    <span>
+                                        {{ comboInfo[3].name }}
+                                    </span>
+                                    <span class=" text-[#f43f5e]">
+                                        {{ comboInfo[3].price_now }}元
+                                    </span>
+                                </div>
+                                <div class="text-[10px]">
+                                    <span class="">
+                                        原价
+                                    </span>
+                                    <del class="">
+                                        {{ comboInfo[3].price_origin }}元
+                                    </del>
+                                </div>
+                            </span>
+                        </span>
+                    </div>
+                    <div class="w-full">
+                        <span class="w-[100px] inline-block ">
+                            应付金额
+                        </span>
+                        <span class="text-[#f43f5e] text-[30px]">
+                            {{ price }}
+                        </span>
+                        <span>
+                            元
+                        </span>
+                    </div>
+                    <div>
+                        <span class="w-[100px] inline-block">
+                            <span>
+                                VIP
+                            </span>
+                            <span>
+                                有效期
+                            </span>
+                        </span>
+                        <span>
+                            <span>
+                                {{ curTime }}
+                            </span>
+                            <span>
+                                -
+                            </span>
+                            <span>
+                                {{ destTime }}
+                            </span>
+                        </span>
+                    </div>
+                    <div class="flex">
+                        <div class="w-[100px]">
+                            支付平台
+                        </div>
+                        <div class="flex justify-center gap-[60px]">
+                            <div class="w-[100px] h-[100px] hover:"
+                                 :class="{ 'border': selectPayMethods, 'border-indigo-600': selectPayMethods, 'drop-shadow-2xl': selectPayMethods }"
+                                 @click="funcSelectPay(1)">
+                                <img src="/pay/WePay.png" class="w-full h-full">
+                            </div>
+                            <div class="w-[100px] h-[100px]"
+                                 :class="{ 'border': !selectPayMethods, 'border-indigo-600': !selectPayMethods, 'drop-shadow-2xl': !selectPayMethods }"
+                                 @click="funcSelectPay(2)">
+                                <img src="/pay/AliPay.png" class="w-full h-full scale-[1]">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="absolute bottom-[40px] flex">
+                        <div class="w-[100px]">
+                        </div>
+                        <div class="flex justify-center">
+                            <span class="px-[20px] py-[10px] bg-[#F4C3C2] rounded-l" @click="funcClickPay">确认付款</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped></style>
