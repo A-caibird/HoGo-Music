d6a33816eff362f5d50c60b21cb32867c41a2881
Acaibird <newcoder@icloud.com>
2024-04-01 00:15:06 +0800

    feat:删除原有router文件,修改main.js内容

--- home/main.js
+++ home/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
-import router from '/src/router/router.js'
+import router from '/router/router.js'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
