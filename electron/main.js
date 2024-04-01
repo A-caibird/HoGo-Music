0eb62b6d778eae5b571488f2c9905a169e7edfa4
Acaibird <newcoder@icloud.com>
2024-04-01 08:35:20 +0800

    fix: modify electron and tauri dev server url

--- electron/main.js
+++ electron/main.js
@@ -9,7 +9,7 @@ const createWindow = () => {
     // mainWindow.loadFile(path.join(__dirname, "./index.html"));
     // 使用 loadURL 加载 http://localhost:3004 ，也就是我们刚才创建的 Vue 项目地址
     // 3004 改为你 Vue 项目的端口号
-    mainWindow.loadURL("http://localhost:5173/src/pages/login/").then(r => console.log(r));
+    mainWindow.loadURL("http://localhost:5173").then(r => console.log(r));
 }
 
 app.whenReady().then(() => {
